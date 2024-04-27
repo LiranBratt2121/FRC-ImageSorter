794d3c3f9863abd654ff2b5d80a90d91d4e10b93
LiranBratt2121 <brattlirannin@gmail.com>
2024-04-27 07:52:25 +0300

    main: Update main for new image data support

--- main.py
+++ main.py
@@ -2,33 +2,42 @@ from ultralytics import YOLO
 from ultralytics.engine.results import Results
 
 from typing import List
+import cv2
 
 from loader import FileLoader
 from image_data import ImageData
-from utils import to_np_array, to_ROI, get_team
+from utils import parse_team, to_ROI, get_team, predict_images
 
 loader = FileLoader(clothes_model_name='clothing_model_best_yolov8m.pt',
-                    shirt_model_name='shirt_model_yolov8m-cls_last.pt')
+                    shirt_model_name='shirt_model_yolov8m-cls_last.pt',
+                    input_dir='input')
 
 clothes_model = YOLO(loader.clothes_detector_model_path)
 shirt_model = YOLO(loader.shirt_detector_model_path)
 
-preprocess_results: List[Results] = clothes_model.predict(source=loader.input_images_path,
-                                               stream=True,
-                                               classes=[0, 1],
-                                               conf=0.4)
+preprocess_results = predict_images(
+    model=clothes_model,
+    imgs_path=loader.input_images_path,
+    classes=[0, 1],
+    conf=0.4)
+
 data: List[ImageData] = []
 
 for preprocess_result in preprocess_results:
-    detections = [to_np_array(tensor.xyxy) for tensor in preprocess_result.boxes]
+    print(f'{preprocess_result=}')
+    detections = preprocess_result[0].numpy().boxes.xyxy
 
-    img = preprocess_result.orig_img
-    image_data = ImageData(image_path=preprocess_result.path)
-    
+    img = preprocess_result[0].orig_img
+    image_data = ImageData(image_path=preprocess_result[0].path)
+
     for detection in detections:
-        roi = to_ROI(img, detection[0])
+        roi = to_ROI(img, detection)
         results: List[Results] = shirt_model.predict(source=roi, conf=0.2)
-
-        image_data.add(*get_team(results.copy()))
+        # cv2.imshow('image', roi)
+        # print(get_team(results))
+        # cv2.waitKey(0)
+        shirt_info = parse_team(get_team(results)[0])
+        
+        image_data.add(*get_team(results), detection, shirt_info.get('year'), f"{shirt_info.get('orientation')}-{shirt_info.get('other_data')}")
 
     data.append(image_data)
