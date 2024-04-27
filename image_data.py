ffaeb65b05184f8617748626701cd17d1b6bc030
LiranBratt2121 <brattlirannin@gmail.com>
2024-04-27 07:51:59 +0300

    image_data: Total rewrite to include more scalable stracture

--- image_data.py
+++ image_data.py
@@ -1,38 +1,82 @@
+import cv2
+from numpy import ndarray
+import openpyxl
+from openpyxl.drawing.image import Image
+
 from typing import Dict, List
+
+from dataclasses import dataclass
+
+from utils import to_ROI
+
+
+@dataclass
+class Shirt:
+    """
+    A data structure representing a detected shirt.
+
+    Attributes:
+      team_name (str): Name of the team the shirt belongs to (without year and info).
+      confidence (float): Confidence level of identifying the Shirt in the image (0.0 to 1.0).
+      roi (ndarray): Region of interest (bounding box) for the member in the image.
+      year (str | None): Year associated with the team (optional).
+      other_data (str | None): Additional information about the team (optional).
+    """
+    team_name: str
+    confidence: float
+    roi: ndarray
+    year: str = None
+    other_data: str = None
+
 
 class ImageData:
     """
-    A data structure containing information about the image.
-    
-    Attributes:
-        teams (List[Dict[str: float | str]]): List of teams in the image.
-        [{
-            "name": "1943",
-            "conf": 0.5    
-        }]
+    A data structure containing information about an image and detected shirts.
 
-        image_path (str): Path to the image.
+    Attributes:
+      image_path (str): Path to the image.
+      teams (Dict[str, List[Shirt]]): Dictionary mapping team names (without year and info) to a list of Shirt objects.
+      team_occurrences (Dict[str, int]): Dictionary mapping team names (without year and info) to their occurrence count.
+
     """
 
-    def __init__(self, image_path) -> None:
-        '''
-        Initialize the ImageData object.
-
-        Args:
-            image_path (str): Path to the target image.
-        '''
-        self.teams: List[Dict[str: float | str]] = []
+    def __init__(self, image_path: str) -> None:
         self.image_path = image_path
-
-    def add(self, name: str, conf: float) -> None:
-        """
-        Adds a team to the teams list if it doesn't already exist.
-        """
-        if not any(team.get('name') == name for team in self.teams):
-            self.teams.append({"name": name, "conf": conf})
+        self.teams: Dict[str, List[Shirt]] = {}
+        self.team_occurrences: Dict[str, int] = {}
 
     def __repr__(self) -> str:
         """
         Returns a string representation of the ImageData object.
         """
-        return f"ImageData(image_path={self.image_path}, teams={self.teams})"
+        return f'ImageData(image_path={self.image_path}, teams={self.teams}, team_occurrences={self.team_occurrences})'
+
+    def add(self, team_name: str, confidence: float, roi: ndarray, year: str = None, other_data: str = None) -> None:
+        """
+        Adds a shirt to the image data with the specified information.
+
+        Args:
+        team_name (str): Name of the team the shirt belongs to (without year and info).
+        confidence (float): Confidence level of identifying the shirt in the image (0.0 to 1.0).
+        roi (ndarray): Region of interest (bounding box) for the shirt in the image.
+        year (str, optional): Year associated with the team. Defaults to None.
+        other_data (str, optional): Additional information about the shirt. Defaults to None.
+        """
+        if team_name not in self.teams:
+            self.teams[team_name] = []
+            self.team_occurrences[team_name] = 0
+            return
+
+        self.teams[team_name].append(Shirt(team_name, confidence, roi, year, other_data))
+        self.team_occurrences[team_name] += 1
+
+    @property
+    def get_team_names(self) -> List[str]:
+        """
+        Returns a list of unique team names (without year and additional info).
+
+        Returns:
+          List[str]: List of team names.
+        """
+        return list(self.teams.keys())
+    
