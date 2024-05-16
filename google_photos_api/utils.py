bc9bc2efbb8540cf4ccf2bb53bcd916eb5fa6b6b
LiranBratt2121 <brattlirannin@gmail.com>
2024-05-16 22:52:38 +0300

    google_photos_api/utils: Add  basic auth util function

---
+++ google_photos_api/utils.py
@@ -0,0 +1,31 @@
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import Resource
+
+from typing import List
+
+def authenticate_api(api_lib: str, version: str, scopes: List[str], secret_file_path: str, port=4040, host='localhost') -> Resource:
+    """
+    Authenticates with the Google Photos API and returns the service object.
+    
+    Args:
+        api_lib (str): Name of the API library to authenticate.
+        version (str): Version of the API.
+        scopes (List[str]): List of scopes required for authentication.
+        secret_file_path (str): Path to the client secret file.
+        port (int): Port to run the local server for authentication. Default is 4040.
+        host (str): Hostname for the local server. Default is 'localhost'.
+        
+    Returns:
+        Resource: Service object for the authenticated API.
+    """
+    # Create flow object
+    flow = InstalledAppFlow.from_client_secrets_file(secret_file_path, scopes)
+
+    # Run the authentication flow
+    credentials = flow.run_local_server(port=port, host=host, redirect_uri_trailing_slash=False)
+
+    # Build the service
+    service = build(api_lib, version, credentials=credentials)
+
+    return service
