dc0bb06189a4ee886bc78567bf677ca38920fc82
LiranBratt2121 <brattlirannin@gmail.com>
2024-05-17 17:55:37 +0300

    Add token saving functionality to authenticate_api

--- google_photos_api/gutils.py
+++ google_photos_api/gutils.py
@@ -1,3 +1,4 @@
+import os
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import Resource
@@ -6,7 +7,7 @@ from typing import List
 
 def authenticate_api(api_lib: str, version: str, scopes: List[str], secret_file_path: str, port=4040, host='localhost') -> Resource:
     """
-    Authenticates with the Google Photos API and returns the service object.
+    Authenticates with the Google Photos API and returns the service object, and saves the auth token as a file.
     
     Args:
         api_lib (str): Name of the API library to authenticate.
@@ -19,13 +20,17 @@ def authenticate_api(api_lib: str, version: str, scopes: List[str], secret_file_
     Returns:
         Resource: Service object for the authenticated API.
     """
-    # Create flow object
+    # Create flow object.
     flow = InstalledAppFlow.from_client_secrets_file(secret_file_path, scopes)
 
-    # Run the authentication flow
+    # Run the authentication flow.
     credentials = flow.run_local_server(port=port, host=host, redirect_uri_trailing_slash=False)
+    
+    # Writes token file.
+    with open(os.path.join(os.getcwd(), 'google_photos_api', 'config', 'token.json'), 'w') as token:
+        token.write(credentials.to_json())
 
-    # Build the service
+    # Build the service.
     service = build(api_lib, version, credentials=credentials, static_discovery=False)
     
     return service
